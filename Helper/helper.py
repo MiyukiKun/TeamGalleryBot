2e204de7aeacec76fdbed0c3f2f06918bc825c50
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-10-10 19:10:03 +0530

    Release V 2.0

--- Helper/helper.py
+++ Helper/helper.py
@@ -4,6 +4,7 @@ def parse_arg(arg):
     arg = arg.replace("_","")
     return arg
 
+
 def parse_about(about):
     unwanted_shit = ["Manga: @Manga_Gallery","Anime: @Anime_Gallery", "Group: @Anime_Discussion_Cafe", "Animes: @Anime_Gallery", "Anime:@Anime_Gallery", "Animes:@Anime_Gallery", "Group:@Anime_Discussion_Cafe", "Group: @MangaTards", "Group:@MangaTards"]
     about = about.split("\n")
