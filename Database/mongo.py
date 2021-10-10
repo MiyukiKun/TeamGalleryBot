cc8e075e8c89dc255741dd2acefc15cadba2619e
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-10-10 19:03:41 +0530

    Add files via upload

---
+++ Database/mongo.py
@@ -0,0 +1,22 @@
+from pymongo.collection import Collection
+from config import client
+
+
+class ChannelsDB:
+    def __init__(self):
+        self.channel_col = Collection(client['AnimeGallery'], 'channelsdata')
+        
+    def find(self, data):
+        return self.channel_col.find_one(data)
+    
+    def full(self):
+        return list(self.channel_col.find())
+
+    def add(self, data):
+        try:
+            self.channel_col.insert_one(data)
+        except:
+            print("value already exists")
+
+    def remove(self, data):
+        self.channel_col.delete_one(data)
